18f39295b8abe284e7974c5143377e3b06150901
salman <s.shahbazi.89@gmail.com>
2022-12-27 16:35:40 +0330

    add out path to gitignore

--- out/production/python_learning/basic_data_structures/MyTree.py
+++ out/production/python_learning/basic_data_structures/MyTree.py
@@ -1,58 +1,62 @@
-# class TreeNode:
-#     def __init__(self, data):
-#         self.data = data
-#         self.children = []
-#         self.parent = None
-#
-#     def get_level(self):
-#         level = 0
-#         current_parent = self.parent
-#
-#         while current_parent is not None:
-#             level += 1
-#             current_parent = self.parent
-#
-#         return level
-#
-#     def add_child(self, data):
-#         child = TreeNode(data)
-#         child.parent = self
-#         self.children.append(child)
-#
-#     def print_tree(self):
-#         space = ' ' * self.get_level() * 3
-#         print(space + self.data)
-#         if self.children:
-#             for child in self.children:
-#                 child.print_tree()
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+    def get_level(self):
+        level = 0
+        current_parent = self.parent
+
+        while current_parent:
+            level += 1
+            current_parent = current_parent.parent
+
+        return level
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+    def print_tree(self, key=None):
+        prefix = '|---'
+        space = ' ' * self.get_level() * 4
+        if key is not None:
+            if key == 'both':
+                values = ' - '.join([str(x) for x in self.data.values()])
+                print(space + prefix + values)
+            else:
+                print(space + prefix + self.data[key])
+        else:
+            print(space + prefix + self.data)
+        if self.children:
+            for child in self.children:
+                child.print_tree(key)
 
 
 if __name__ == "__main__":
-    print("hell")
-    print("fuck")
-    # root = TreeNode("Electronics")
-    #
-    # laptop = TreeNode("Laptop")
-    #
-    # root.children.append(laptop)
-    # apple = TreeNode("Apple")
-    # dell = TreeNode("Dell")
-    # laptop.children.append(apple)
-    # laptop.children.append(dell)
-    # laptop.children.append(dell)
-    # tv = TreeNode("Tv")
-    # samsung = TreeNode("Samsung")
-    # lg = TreeNode("LG")
-    # tv.children.append(samsung)
-    # tv.children.append(lg)
-    # mobile = TreeNode("Mobile")
-    # nokia = TreeNode("Nokia")
-    # xiaomi = TreeNode("Xiaomi")
-    # mobile.children.append(nokia)
-    # mobile.children.append(xiaomi)
-    #
-    # root.children.append(laptop)
-    # root.children.append(tv)
-    # root.children.append(mobile)
+    root = TreeNode("Electronics")
 
-    # root.print_tree()
+    laptop = TreeNode("Laptop")
+    apple = TreeNode("Apple")
+    dell = TreeNode("Dell")
+    laptop.add_child(apple)
+    laptop.add_child(dell)
+
+    tv = TreeNode("Tv")
+    samsung = TreeNode("Samsung")
+    lg = TreeNode("LG")
+    tv.add_child(samsung)
+    tv.add_child(lg)
+
+    mobile = TreeNode("Mobile")
+    nokia = TreeNode("Nokia")
+    xiaomi = TreeNode("Xiaomi")
+    mobile.add_child(nokia)
+    mobile.add_child(xiaomi)
+
+    root.add_child(laptop)
+    root.add_child(tv)
+    root.add_child(mobile)
+
+    root.print_tree()
